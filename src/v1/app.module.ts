35213623481241f9541afd867790ae2f9ee31f78
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2023-03-26 08:36:55 +0100

    feat: added database configuration validation using joi

--- src/v1/app.module.ts
+++ src/v1/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './modules/categories/category.module';
-import { PostsModule } from './modules/posts/post.module';
-import { UsersModule } from './modules/users/user.module';
+import { CategoriesModule } from './modules/category.module';
+import { PostsModule } from './modules/post.module';
+import { UsersModule } from './modules/user.module';
 import { DataSource } from 'typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/typeorm.module';
+import { configValidationSchema } from './database/typeorm.config.validation';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configValidationSchema,
+    }),
     DatabaseModule,
     UsersModule,
     PostsModule,
--- src/v1/database/typeorm.module.ts
+++ src/v1/database/typeorm.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from '../modules/users/user.entity';
+import { User } from '../entities/user.entity';
 
 @Module({
   imports: [
